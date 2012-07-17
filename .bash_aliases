6387745d5b161f3a9d60d4e5bdc87b28a925941c
TyOverby <ty@pre-alpha.com>
2012-07-17 00:49:31 -0700

    split bash profile up into multiple files

---
+++ .bash_aliases
@@ -0,0 +1,4 @@
+alias la="ls -a"
+alias ls='ls -CF --color=auto --ignore="NTUSER.*" --ignore="ntuser.*"'
+alias setdir='pwd > ~/.curdir'
+alias go='cat ~/.curdir | cd'
--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 cd ~
-ln -s dotfiles/.bashrc .bashrc 
-ln -s dotfiles/.gitconfig .gitconfig
 
-git clone git@github.com:TyOverby/Vim-Config.git ~/.vim
-cd ~/.vim
-sh install.sh
+for file in ~/dotfiles/.{bash_aliases,bash_cygwin,bash_prompt,bashrc,gitconfig};
+do
+  echo "linking $file  " `basename $file`
+  ln -s "$file" `basename $file`
+done
+unset file
+
+#git clone git@github.com:TyOverby/Vim-Config.git ~/.vim
+#cd ~/.vim
+#sh install.sh
